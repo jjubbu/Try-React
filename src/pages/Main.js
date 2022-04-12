e1db23b47a88963b29a8d66cb461badbb2d24a18
jjubbu <hogugugut4102.off@gmail.com>
2022-04-13 01:31:38 +0900

    커밋테스트 진짜 최종

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,6 +3,7 @@ import { history } from "../redux/store";
 //텟테테렛테..
 //이번에도 중복된다면 나는 바지에 똥을 쌀거야
 //제발 이번이 마지막이길~~
+//이번이 진짜 제발 마지막이길~~~
 const Main = () => {
   const goto = (e) => {
     const name = e.target.getAttribute("name");
